181c7f9b2794abff16d3be7049b5c3f6c8736ab3
SimonLovch <57564709+SimonLovch@users.noreply.github.com>
2021-11-15 22:38:18 +0700

    Add files via upload

---
+++ ThreeInARow/GameView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+
+namespace ThreeInARow
+{
+    
+    public partial class GameView
+    {
+        private Field game;
+
+        public GameView()
+        {
+            InitializeComponent();
+            StartGame();
+        }
+        
+        private void StartGame()
+        {
+            int maxTime = 60;
+            game = new Field(Canvas, ScoreLabel);
+            timer = new Timer(game, TimeBar, TimeLabel, maxTime);
+        }
+    }
+}
